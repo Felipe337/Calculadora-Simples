fa1ddd7f71df4df97a3e91611877bd99b66a9bea
Felipe337 <116205272+Felipe337@users.noreply.github.com>
2024-08-16 12:47:38 -0400

    Update func.h
    Update func.h

--- func.h
+++ func.h
@@ -1,19 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "func.h"
 
-float somar(float a, float b) {
-    return a + b; }
+float somar(float a, float b);
+float subtrair(float a, float b);
+float multiplicar(float a, float b);
+float dividir(float a, float b);
 
-float subtrair(float a, float b) {
-    return a - b; }
-
-float multiplicar(float a, float b) {
-    return a * b; }
-
-float dividir(float a, float b) {
-    if (b != 0) {
-        return a / b; }
-            else {
-                printf("Erro: Divisão por zero!\n");
-                exit(EXIT_FAILURE); } }
